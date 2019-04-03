6759a945f34e369d105872c427543e9b7c56ea81
201401660 <201401660@inu.ac.kr>
2019-04-03 14:37:03 +0900

    change background image & apply contrast & correct detect contour

--- 01_crop.py
+++ 01_crop.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
+# crop template
 
 import os
-import cv2
-import numpy as np
-
-# must import if working with opencv in python
 import numpy as np
 import cv2
+from PIL import Image
+from PIL import ImageEnhance
 
+
+# list division
 def chunksList(List,size):
     for i in range(0, len(List), size):
         yield List[i : i + size]
@@ -20,14 +21,19 @@ def crop (img_dir):
 
     imgList = sorted(os.listdir(img_dir))
 
+    # unicode list matching template image
     idx = 0
     f = open('399-uniform.txt', 'r')
     charList = f.readlines()
 
+    # case1
     for i in range(len(charList)):
         charList[i] = charList[i].strip()  # strip : delete '\n'
 
+    # case2
+    # charList = [i.strip() for i in charList]
 
+    # crop image
     for img in imgList:
         image = cv2.imread(os.path.join(img_dir,img))
 
@@ -55,60 +61,114 @@ def crop (img_dir):
         # cv2.waitKey(0)
 
         # find contours
-        ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        # opencv > 4.0 ------------ ctrs,_ =
+        # opencv < 4.0 ------------ _, ctrs, _ =
+        ctrs, _ = cv2.findContours(img_dilation.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-        # sort contours - 안해도 상관없음
-        sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
 
         cnt = 0
         rect = []
 
-        for i, ctr in enumerate(sorted_ctrs):
-            # Get bounding box
-
+        for i, ctr in enumerate(ctrs):
+            # Get bounding
             x, y, w, h = cv2.boundingRect(ctr)
 
-            print("H", h / float(H), "w", w / float(W))
+            #print("H", h / float(H), "w", w / float(W))
 
             # Getting ROI
-            roi = image[y:y + h, x:x + w]
+            #roi = image[y:y + h, x:x + w]
 
             # show ROI
             # cv2.imshow('segment no:'+str(i),roi)
 
             if 0.045 <= h / float(H) < 0.9 and 0.065 <= w / float(W) < 0.9:
                 rect.append([x, y, w, h])
-                print('xywh:', [x, y, w, h])
+                #print('xywh:', [x, y, w, h])
 
                 cnt = cnt + 1
 
         # [ x,y,w,h ] 중 y 값에 대해 정렬 => row 만큼 묶을 수 있게 됨
         rect = sorted(rect, key=lambda k: [k[1]])
 
-        print('selected rectangle:', rect)
-
         rect = list(chunksList(rect, 12))  # 12개(= 1 row )만큼씩 묶어줌
 
-        # case2
-        # charList = [i.strip() for i in charList]
-
         for row in rect:
-            print(row)
+            #print(row)
             row = sorted(row, key=lambda k: [k[0]])  # row별로 column(=x 좌표)에 대해 정렬
             for rec in row:
+
                 x, y, w, h = rec
-                roi = image[y:y + h + 1, x:x + w + 1]  # 1 is margin
+                roi = thresh[y:y + h + 1, x:x + w + 1]  # 1 is margin
 
-                cv2.namedWindow('marked areas', cv2.WINDOW_NORMAL)
-                cv2.imshow('marked areas', roi)
-                cv2.waitKey(10)
+                # To make contours on purpose
+                open_kernel = np.ones((15,15), np.uint8)
+                dilated_image = cv2.dilate(roi, open_kernel, iterations=1)
+                dilated_image = ~dilated_image
+                contours, _ = cv2.findContours(dilated_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+                #_r = sorted([ cv2.contourArea(contour) for contour in enumerate(contours)])[-2]
+                #_r = sorted([cv2.contourArea(r) for r in contours])[-2]
 
-                cv2.rectangle(image, (x, y), (x + w, y + h), (171, 255, 0), 3)
-                cv2.namedWindow('marked areas', cv2.WINDOW_NORMAL)
-                cv2.imshow('marked areas', image)
-                cv2.waitKey(0)
+                # not save image not detected contour
+                try:
+                    _r = ([contour for contour in contours])[-2]
 
-                #cv2.imwrite('/home/malab2/PycharmProjects/FONTS/output/' + charList[idx] + '.png', roi)
+                except:
+
+                    idx += 1
+                    continue
+
+                _x, _y, _w, _h = cv2.boundingRect(_r)
+                # img1 = cv2.rectangle(dilated_image, (_x, _y), (_x + _w, _y + _h), 7)
+                # cv2.imshow('test',img1)
+
+                # make empty white image
+                background = np.zeros([128,128],dtype=np.uint8)
+                background.fill(255)
+
+                _roi = gray[y:y + h + 1, x:x + w + 1]
+
+                # cv2.rectangle(image, (x, y), (x + w, y + h), (171, 255, 0), 3)
+                # cv2.namedWindow('marked areas', cv2.WINDOW_NORMAL)
+                # cv2.imshow('marked areas', image)
+                # cv2.waitKey(0)
+
+
+                cropped_image = _roi[_y:_y+_h, _x:_x+_w]
+
+
+                # cv2.rectangle(_roi, (_x, _y), (_x + _w, _y + _h), (100, 100, 100), 1)
+                # cv2.namedWindow('cropped areas', cv2.WINDOW_NORMAL)
+                # cv2.imshow('cropped areas', _roi)
+                # cv2.waitKey(0)
+
+
+                # resize according to width and height
+                if _w > _h:
+                    height_size = 90 * _h / _w
+                    #print('h',height_size)
+                    cropped_image = cv2.resize(cropped_image,(90, height_size))
+                    background[int(64 - height_size / 2):int(64 - height_size / 2)+ cropped_image.shape[0],19:19+ cropped_image.shape[1]] = cropped_image
+                else:
+                    width_size = 90 * _w / _h
+                    #print('w',width_size)
+                    cropped_image = cv2.resize(cropped_image,(width_size, 90))
+                    background[19:19+cropped_image.shape[0], int(64 - width_size / 2):int(64 - width_size / 2)+ cropped_image.shape[1]] = cropped_image
+
+
+                # Processing to make the writing clearer
+                img = Image.fromarray(background)
+                enhancerimg = ImageEnhance.Contrast(img)
+                _image = enhancerimg.enhance(1.5)
+                opencv_image = np.array(_image)
+                opencv_image = cv2.bilateralFilter(opencv_image, 9, 30, 30)
+
+
+                cv2.imshow('back',opencv_image)
+                cv2.waitKey(1)
+
+                # Save image
+                cv2.imwrite('/home/malab2/PycharmProjects/FONTS/template_YJ/output_YJ/uni' + charList[idx] + '.png', opencv_image)
+
                 idx += 1
 
                 if idx == 399:
@@ -117,4 +177,6 @@ def crop (img_dir):
 
 
 if __name__ == '__main__':
-    crop('/home/malab2/PycharmProjects/FONTS/template')
+    crop('/home/malab2/PycharmProjects/FONTS/template_YJ')
+    #crop('/home/yejin/PycharmProjects/font/template')
+
