6178be145de719f5dbb43e342857eb4ceb388f2a
201401660 <201401660@inu.ac.kr>
2019-04-03 13:48:35 +0900

    change background image & apply contrast

--- 01_crop.py
+++ 01_crop.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import cv2
+import numpy as np
+
 # must import if working with opencv in python
 import numpy as np
 import cv2
-
 
 def chunksList(List,size):
     for i in range(0, len(List), size):
@@ -38,7 +40,6 @@ def crop (img_dir):
         # cv2.waitKey(0)
 
         # binary
-        #blur = cv2.bilateralFilter(gray, 9, 75, 75)
         ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 
         # cv2.namedWindow('second', cv2.WINDOW_NORMAL)
@@ -54,37 +55,37 @@ def crop (img_dir):
         # cv2.waitKey(0)
 
         # find contours
-        _, ctrs, _ = cv2.findContours(img_dilation.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         # sort contours - 안해도 상관없음
-        #sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+        sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
 
         cnt = 0
         rect = []
 
-        for i, ctr in enumerate(ctrs):
-            # Get bounding
+        for i, ctr in enumerate(sorted_ctrs):
+            # Get bounding box
 
             x, y, w, h = cv2.boundingRect(ctr)
 
-            #print("H", h / float(H), "w", w / float(W))
+            print("H", h / float(H), "w", w / float(W))
 
             # Getting ROI
-            #roi = image[y:y + h, x:x + w]
+            roi = image[y:y + h, x:x + w]
 
             # show ROI
             # cv2.imshow('segment no:'+str(i),roi)
 
             if 0.045 <= h / float(H) < 0.9 and 0.065 <= w / float(W) < 0.9:
                 rect.append([x, y, w, h])
-                #print('xywh:', [x, y, w, h])
+                print('xywh:', [x, y, w, h])
 
                 cnt = cnt + 1
 
         # [ x,y,w,h ] 중 y 값에 대해 정렬 => row 만큼 묶을 수 있게 됨
         rect = sorted(rect, key=lambda k: [k[1]])
 
-        #print('selected rectangle:', rect)
+        print('selected rectangle:', rect)
 
         rect = list(chunksList(rect, 12))  # 12개(= 1 row )만큼씩 묶어줌
 
@@ -92,51 +93,22 @@ def crop (img_dir):
         # charList = [i.strip() for i in charList]
 
         for row in rect:
-            #print(row)
+            print(row)
             row = sorted(row, key=lambda k: [k[0]])  # row별로 column(=x 좌표)에 대해 정렬
             for rec in row:
-
                 x, y, w, h = rec
-                roi = thresh[y:y + h + 1, x:x + w + 1]  # 1 is margin
-                open_kernel = np.ones((15,15), np.uint8)
-                dilated_image = cv2.dilate(roi, open_kernel, iterations=1)
-                dilated_image = ~dilated_image
-                _, contours, _ = cv2.findContours(dilated_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-                #_r = sorted([ cv2.contourArea(contour) for contour in enumerate(contours)])[-2]
-                #_r = sorted([cv2.contourArea(r) for r in contours])[-2]
-                _r = ([contour for contour in contours])[-2]
-                _x, _y, _w, _h = cv2.boundingRect(_r)
-                #img1 = cv2.rectangle(dilated_image, (_x, _y), (_x + _w, _y + _h), 7)
-                #cv2.imshow('test',img1)
+                roi = image[y:y + h + 1, x:x + w + 1]  # 1 is margin
 
-                #open_kernel = np.ones((2,2), np.uint8)
-                #dilated_image = cv2.dilate(roi, open_kernel, iterations=1)
-                #blur = cv2.bilateralFilter(roi, 9,75,75)
-                #opening = cv2.morphologyEx(blur, cv2.MORPH_OPEN, open_kernel)
-                #cv2.imshow('opening',img1)
-                #cv2.waitKey(0)
+                cv2.namedWindow('marked areas', cv2.WINDOW_NORMAL)
+                cv2.imshow('marked areas', roi)
+                cv2.waitKey(10)
 
-                # resize according to width and height
-                background = np.ones((128, 128)) * 255
-                cropped_image = ~roi[_y:_y+_h, _x:_x+_w]
-                if _w > _h:
-                    height_size = 90 * _h / _w
-                    print('h',height_size)
-                    cropped_image = cv2.resize(cropped_image,(90, height_size))
-                    print(cropped_image.shape[0], cropped_image.shape[1])
-                    background[int(64 - height_size / 2):int(64 - height_size / 2)+ cropped_image.shape[0],19:19+ cropped_image.shape[1]] = cropped_image
-                else:
-                    width_size = 90 * _w / _h
-                    print('w',width_size)
-                    cropped_image = cv2.resize(cropped_image,(width_size, 90))
-                    print(cropped_image.shape[0],cropped_image.shape[1])
-                    background[19:19+cropped_image.shape[0], int(64 - width_size / 2):int(64 - width_size / 2)+ cropped_image.shape[1]] = cropped_image
+                cv2.rectangle(image, (x, y), (x + w, y + h), (171, 255, 0), 3)
+                cv2.namedWindow('marked areas', cv2.WINDOW_NORMAL)
+                cv2.imshow('marked areas', image)
+                cv2.waitKey(0)
 
-                #result = cv2.bilateralFilter(background, 9, 75, 75)
-                cv2.imshow('back',background)
-                cv2.waitKey(1)
-
-                cv2.imwrite('/home/yejin/PycharmProjects/font/output/uni' + charList[idx] + '.png', background)
+                #cv2.imwrite('/home/malab2/PycharmProjects/FONTS/output/' + charList[idx] + '.png', roi)
                 idx += 1
 
                 if idx == 399:
@@ -145,6 +117,4 @@ def crop (img_dir):
 
 
 if __name__ == '__main__':
-    #crop('/home/malab2/PycharmProjects/FONTS/image')
-    crop('/home/yejin/PycharmProjects/font/template')
-
+    crop('/home/malab2/PycharmProjects/FONTS/template')
